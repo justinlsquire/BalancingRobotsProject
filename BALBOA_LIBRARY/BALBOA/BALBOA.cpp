77697fe2f933cc5f2a798270eb065a370c56a52e
Mattias Adolfsson <matte216@users.noreply.github.com>
2019-07-25 15:11:14 +0200

    Copy MINSEG_LIBRARY into BALBOA_LIBRARY

---
+++ BALBOA_LIBRARY/BALBOA/BALBOA.cpp
@@ -0,0 +1,259 @@
+﻿/*
+  MINSEG.cpp - Library for using MinSeg with balancing and estimation library
+*/
+
+#include "Arduino.h"
+#include "BALBOA.h"
+
+
+// initialize static variables here
+volatile long Minseg::encCnt;
+MPU6050 Minseg::accelgyro;
+
+
+Minseg::Minseg(){
+	
+	// initialize some stuff
+	gx_scale = GYRO_SCALE_DEFAULT;
+	gy_scale = GYRO_SCALE_DEFAULT;
+	gz_scale = GYRO_SCALE_DEFAULT;
+	
+	ax_scale = ACCEL_SCALE_DEFAULT;
+	ay_scale = ACCEL_SCALE_DEFAULT;
+	az_scale = ACCEL_SCALE_DEFAULT;
+	
+	maxVoltage = MAX_VOLTAGE_DEFAULT;
+	
+	
+} // end of constructor
+
+
+void Minseg::setupHardware(void){
+	// setup LED for debugging and heartbeat 
+	pinMode(LED_PIN,OUTPUT);
+	digitalWrite(LED_PIN,1);
+	
+	// setup Motor ports and pins
+	pinMode(MTR_PWM_PIN,OUTPUT); // PWM pin for motor driver
+	pinMode(MTR_DIR_PIN,OUTPUT); // direction pin for motor driver
+
+	// set at 0 initially
+	digitalWrite(MTR_DIR_PIN,LOW);
+	analogWrite(MTR_PWM_PIN,0);	
+	
+	
+	// setup IMU object
+	// begin wire (i2c) 
+	Wire.begin();
+	
+	if (accelgyro.testConnection())
+	{
+		accelgyro.setDLPFMode(5);
+		accelgyro.initialize();
+		_imuAvailable = 1;
+#ifdef DEBUG		
+		Serial.println("imu is connected");	
+#endif		
+	}
+	else
+	{
+		_imuAvailable = 0;
+#ifdef DEBUG
+		Serial.println("imu is NOT connected");
+#endif		
+	}
+	
+	// setup encoder ports and pins, and ISR
+	// encoder is on pins 2 and 3
+	// set up pullup resistors to prevent floating
+	pinMode(ENC1_PIN,INPUT_PULLUP);
+	pinMode(ENC2_PIN,INPUT_PULLUP);
+
+	attachInterrupt(digitalPinToInterrupt(ENC1_PIN),Minseg::enc1ISR,CHANGE);
+	attachInterrupt(digitalPinToInterrupt(ENC2_PIN),Minseg::enc2ISR,CHANGE);	
+	
+} // end of setupHardware
+
+void Minseg::toggleLED(void){
+	LED_PORT ^= (1<<LED_PORT_OFFSET);
+} // end of toggleLED
+
+void Minseg::setMotorPWM(int pwmVal){
+	uint8_t tempMtrOut;
+	
+	// update motor command
+	if (pwmVal < 0)
+	{
+		// set direction 0
+		MTR_DIR_PORT &= ~(1<<MTR_DIR_OFFSET);
+		tempMtrOut = -pwmVal;
+	}
+	else
+	{
+		// set direction 1
+		MTR_DIR_PORT |= (1<<MTR_DIR_OFFSET);
+		tempMtrOut = 255-pwmVal;
+	}
+
+	// write PWM
+	analogWrite(MTR_PWM_PIN,tempMtrOut);	
+} // end of setMotorPWM
+
+
+void Minseg::enc1ISR(void){
+	
+	// temporary
+	LED_PORT ^= (1<<LED_PORT_OFFSET);
+	
+	// read the whole register
+	uint8_t tempReg = ENC_READ_PORT;
+	// read the pin to see if it is high or low
+	if (tempReg & (1<<ENC1_PORT_OFFSET))
+	{
+		// low to high transition happened on enc pin 1
+		// check enc pin 2
+		if (tempReg & (1<<ENC2_PORT_OFFSET))
+		{
+			// enc pin 2 is high - moving negative direction
+			encCnt--;
+		}
+		else
+		{
+			// enc pin 2 is low - moving positive direction
+			encCnt++;
+		}
+	} // if (tempReg & (1<<ENC1_PORT_OFFSET))
+	else
+	{
+		// high to low transition happened on enc pin 1
+		// check enc pin 2
+		if (tempReg & (1<<ENC2_PORT_OFFSET))
+		{
+			// enc pin 2 is high - moving negative direction
+			encCnt++;
+		}
+		else
+		{
+			// enc pin 2 is low - moving positive direction
+			encCnt--;
+		}
+	} // else - from if (tempReg & (1<<ENC1_PORT_OFFSET))	
+} // end of enc1ISR
+
+
+void Minseg::enc2ISR(void){
+	
+	// temporary
+	LED_PORT ^= (1<<LED_PORT_OFFSET);
+	
+	// read the whole register
+	uint8_t tempReg = ENC_READ_PORT;
+	// read the pin to see if it is high or low
+	if (tempReg & (1<<ENC2_PORT_OFFSET))
+	{
+		// low to high transition happened on enc pin 1
+		// check enc pin 2
+		if (tempReg & (1<<ENC1_PORT_OFFSET))
+		{
+			// enc pin 2 is high - moving negative direction
+			encCnt++;
+		}
+		else
+		{
+			// enc pin 2 is low - moving positive direction
+			encCnt--;
+		}
+	} // if (tempReg & (1<<ENC1_PORT_OFFSET))
+	else
+	{
+		// high to low transition happened on enc pin 1
+		// check enc pin 2
+		if (tempReg & (1<<ENC1_PORT_OFFSET))
+		{
+			// enc pin 2 is high - moving negative direction
+			encCnt--;
+		}
+		else
+		{
+			// enc pin 2 is low - moving positive direction
+			encCnt++;
+		}
+	} // else - from if (tempReg & (1<<ENC1_PORT_OFFSET))	
+} // end of enc2ISR
+
+uint8_t Minseg::imuStatus(void){
+	// returns the status of the IMU
+	return _imuAvailable;
+} // end of imuStatus
+
+
+int16_t Minseg::getAccYRaw(void){
+	return accelgyro.getAccelerationY();
+} // and of getAccY
+
+int16_t Minseg::getAccZRaw(void){
+	return accelgyro.getAccelerationZ();
+} // and of getAccZ
+
+int16_t Minseg::getGyroXRaw(void){
+	return accelgyro.getRotationX();
+} // and of getAccY
+
+void Minseg::updateAccYg(void){
+	ay = (float)(Minseg::getAccYRaw() - ay_raw_offset) * ay_scale;
+} // end of updateAccYg
+
+void Minseg::updateAccZg(void){
+	az = (float)(Minseg::getAccZRaw() - az_raw_offset) * az_scale;
+} // end of updateAccZg
+
+void Minseg::updateGyroXdps(void){
+	gx = (float)(Minseg::getGyroXRaw() - gx_raw_offset) * gx_scale;
+} // end of updateGyroXdps
+
+void Minseg::updateGyro(void){
+	Minseg::updateGyroXdps();
+	//Minseg::updateGyroYdps(); // for example - if more gyro values desired
+} // end of updateGyro
+
+void Minseg::updateAccel(void){
+	Minseg::updateAccYg();
+	Minseg::updateAccZg();
+	//Minseg::updateAccXg(); // if this is ever important - needs defined
+} // end of updateAccel
+
+void Minseg::updateEncoders(void){
+	// get current time
+	
+	// get time difference
+	
+	// convert counts to revolutions
+	
+	// divide revolutions by time difference
+	
+	// integrate this to add on to the displacement
+	x1 = 2.0; // temporary - for testing
+} // end of updateEncoders
+
+
+void Minseg::updateMotor1(float Vin){
+	// convert to percentage of max
+	
+	// simplified - do better job in the future of checking 
+	int16_t tempDuty;
+	
+	tempDuty = (int16_t)(Vin/maxVoltage * 255.0);
+	
+	if (tempDuty > 255)
+	{
+		tempDuty = 255;
+	}
+	else if (tempDuty < (-255))
+	{
+		tempDuty = -255;
+	}
+	
+	// update the actual PWM
+	Minseg::setMotorPWM(tempDuty);
+	
+} // end of updateMotor1
---
+++ BALBOA_LIBRARY/BALBOA/BALBOA.h
@@ -0,0 +1,140 @@
+﻿/*
+  MINSEG.h - Library for using MinSeg with balancing and estimation library
+*/
+#ifndef MINSEG_h
+#define MINSEG_h
+
+#include "Arduino.h"
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+
+
+// define macros here
+
+// debug flag
+#define DEBUG 1
+
+
+#define MTR_DIR_PIN 4
+#define MTR_PWM_PIN 5
+#define MTR_DIR_PORT PORTG
+#define MTR_DIR_OFFSET 5 // PG5 - digital pin 4
+
+#define ENC1_PIN 2
+#define ENC2_PIN 3
+#define ENC_READ_PORT PINE
+#define ENC1_PORT_OFFSET 4 // PE4 - digital pin 2
+#define ENC2_PORT_OFFSET 5 // PE5 - digital pin 3
+
+#define LED_PIN 13 // digital pin 13
+#define LED_PORT PORTB // PB7
+#define LED_PORT_OFFSET 7 // PB7 - digital pin 13
+
+
+// SCALING FACTORS
+#define GYRO_SCALE_DEFAULT 0.0001332312406 // converts to rad/s
+#define ACCEL_SCALE_DEFAULT 0.00006103515625 // converts to g
+
+
+#define MAX_VOLTAGE_DEFAULT 6.0
+
+class Minseg
+{
+  public:
+	static volatile long encCnt; // declare
+	static MPU6050 accelgyro;
+	
+	// displacement related
+	float wheelRadius; // radius (in meters) of wheel(s) used
+	float gearRatio; // total gear ratio from motor output to wheel output
+	float x1; // displacement of motor 1 (left/default)
+	float x2; // displacement of motor 2 (right)
+	
+	// IMU related
+	uint8_t hasAccel; // flag for whether or not it has accelerometer
+	uint8_t hasMagnetometer; // flag for whether or not it has magnetometer
+
+	// gyro
+	int16_t gx_raw, gy_raw, gz_raw; // raw (integer) values of gyro
+	int16_t gx_raw_offset, gy_raw_offset, gz_raw_offset; // raw (integer) offset values of gyro
+	float gx_scale, gy_scale, gz_scale; // scale factors for converting raw gyro values to g* (rad/s)
+	float gx, gy, gz; // gyro values (in rad/s)
+
+	// accelerometer
+	int16_t ax_raw, ay_raw, az_raw; // raw (integer) values of accelerometer
+	int16_t ax_raw_offset, ay_raw_offset, az_raw_offset; // raw (integer) offset values of accelerometer
+	float ax_scale, ay_scale, az_scale; // scale factor for converting a*_raw to a* (in g);
+	float ax, ay, az; // acceleration values (in g)
+
+	// magnetometer
+	int16_t mx_raw, my_raw, mz_raw; // raw (integer) values of magnetometer
+	int16_t mx_raw_offset, my_raw_offset, mz_raw_offset; // raw (integer) offset values of magnetometer
+	float mx_scale, my_scale, mz_scale; // scale factor for converting m*_raw to m* (gauss)
+	float mx, my, mz;
+	
+	// motor related
+	
+	float maxVoltage; // max motor voltage
+	uint8_t hasEncoders; // flag - 0 = no encoder(s), 1 = encoder(s)
+	
+	// # 1 (Right) Motor
+	uint8_t mtr1Active; // 0 = inactive, 1 = active
+	uint8_t mtr1Direction; // 0 = normal, 1 = inverted
+	uint8_t mtr1DutyCycle; // duty cycle of motor for pwm
+	float mtr1Speed; // angular speed of motor in rad/s
+    long enc1counts; // counts (+ or -) of the encoder pulses
+    uint16_t enc1cpr; // counts per revolution of motor
+    uint8_t enc1Direction; // 0 = normal, 1 = inverted	
+	float mtr1Voltage;
+		
+	// # 2 (Left) Motor
+	uint8_t mtr2Active; // 0 = inactive, 1 = active
+	uint8_t mtr2Direction; // 0 = normal, 1 = inverted
+	uint8_t mtr2DutyCycle; // duty cycle of motor for pwm
+	float mtr2Speed; // angular speed of motor in rad/s			
+    long enc2counts; // counts (+ or -) of the encoder pulses
+    uint16_t enc2cpr; // counts per revolution of motor
+    uint8_t enc2Direction; // 0 = normal, 1 = inverted	
+	float mtr2Voltage;
+	
+	
+	
+	
+	// everything else is hard-coded
+	// only thing we need to set up for now is the led pin
+    Minseg();
+	void setupHardware();
+	void toggleLED();
+	void setMotorPWM(int pwmVal);
+	static void enc1ISR();
+	static void enc2ISR();
+	uint8_t imuStatus();
+	
+	// only adding a few of these, but they can be expanded 
+	// to include way more
+	int16_t getGyroXRaw();
+	int16_t getAccYRaw();
+	int16_t getAccZRaw();
+	
+	// get the scaled values of the raw values
+	void updateGyroXdps();
+	void updateAccYg();
+	void updateAccZg();
+	void updateGyro(); // for doing all at once
+	void updateAccel(); // for doing all at once
+	void updateEncoders();
+	
+	void updateMotor1(float Vin);
+	
+  private:
+    int _pwmVal;
+	uint8_t _imuAvailable;
+	//static long encCnt;
+	
+	
+};
+
+#endif
